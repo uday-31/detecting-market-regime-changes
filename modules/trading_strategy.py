0234babf5382c61e68dff0e48e96c51ced445725
uday-31 <uday1577@gmail.com>
2023-02-24 18:15:34 -0500

    implement trading strategy

--- modules/trading_strategy.py
+++ modules/trading_strategy.py
@@ -12,9 +12,21 @@ def mdd(df):
     return ((df.cumsum()-df.cumsum().cummax()).min())
 
 def get_metrics(df):
-    metrics = {'pnl':round(df.sum(),2),
+    metrics = {'pnl':round((1+df).product() - 1,2),
               'sharpe':sharpe(df).round(2),
               'sortino':sortino(df).round(2),
               'volatility':round(df.std(),4),
               'mdd':mdd(df)}
     return pd.DataFrame(metrics, index=[''])
+
+def RSI(price, lookback = 14):
+    # based on: https://blog.quantinsti.com/build-technical-indicators-in-python/
+    ret = price.diff()
+
+    high = ret.clip(lower=0)
+    low = -ret.clip(upper=0)
+    
+    avg_up = high.ewm(alpha = 1/lookback, min_periods = lookback).mean()
+    avg_down = low.ewm(alpha = 1/lookback, min_periods = lookback).mean()
+
+    return (100 - (100/(1 + (avg_up / avg_down))).dropna()).shift(1).dropna()
