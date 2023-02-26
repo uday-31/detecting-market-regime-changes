cdf82c9fb77b1c474ef0895e87571798c984c30a
uday-31 <uday1577@gmail.com>
2023-02-26 18:46:23 -0500

    temp changes

--- modules/hidden_markov_model.py
+++ modules/hidden_markov_model.py
@@ -4,7 +4,7 @@ from hmmlearn import hmm
 import matplotlib.pyplot as plt
 plt.style.use('seaborn')
 
-def fit_hmm(n_components: int, price: pd.Series, indicator: pd.Series, ticker: str, plot: bool =False) -> tuple[pd.Series, hmm.GaussianHMM]:
+def fit_hmm(n_components: int, price: pd.Series, indicator: pd.Series, ticker: str, plot: bool =False, verbose: bool = False) -> tuple[pd.Series, hmm.GaussianHMM]:
     """Fits a Hidden Markov model to the data and predicts regimes on it. Optionally makes a plot.
 
     Args:
@@ -13,20 +13,20 @@ def fit_hmm(n_components: int, price: pd.Series, indicator: pd.Series, ticker: s
         indicator (pd.Series): indicator series we wish to fit the model on
         ticker (str): ticker of the instrument
         plot (bool, optional): whether the regimes need to be plotted. Defaults to False.
+        verbose (bool, optional): whether debugging output needs to be printed. Defaults to False.
 
     Returns:
         tuple[pd.Series,hmm.GaussianHMM]: the predicted regimes and the HMM model
     """
     
     X = indicator.to_numpy().reshape(-1,1)
-    X_train = X[:int(0.7*len(X))] # TODO remove hardcode
 
     models, scores = [], []
     for idx in range(10):
         model = hmm.GaussianHMM(n_components=2, covariance_type="full", n_iter=1000,
             random_state=idx)
   
-        model.fit(X_train)
+        model.fit(X)
         models.append(model)
         scores.append(model.score(X))
 
@@ -34,8 +34,9 @@ def fit_hmm(n_components: int, price: pd.Series, indicator: pd.Series, ticker: s
 
     regimes = pd.Series(model.predict(X))
     regimes.index = indicator.index
+    print(regimes)
 
-    regimes = standardize_regime_labels(regimes)
+    regimes = standardize_regime_labels(regimes,verbose=verbose)
 
     if plot:
         fig, ax = plt.subplots()
@@ -44,7 +45,6 @@ def fit_hmm(n_components: int, price: pd.Series, indicator: pd.Series, ticker: s
 
         for time_start, time_end, regime in zip(regimes.index[:-1], regimes.index[1:], regimes.values[:-1]):
             ax.axvspan(time_start,time_end, alpha=0.8, color=clr[regime])
-        ax.vlines(price.index[int(0.7*len(price))],0,price.max(),color='blue')
         ax.set_title(f"regimes for {ticker}")
         ax.set_ylabel("price")
         plt.show()
@@ -63,22 +63,29 @@ def standardize_regime_labels(regimes: pd.Series, verbose: bool = True) -> pd.Se
     """
     start = regimes.index[0]
     initial_regime = regimes[0]
+    prev_regime = regimes[0] # extra
     total_duration_in_initial_regime = 0
     in_second_regime = False
     if len(np.unique(regimes)) == 1:
         total_duration_in_initial_regime = (regimes.index[-1] - regimes.index[0]).total_seconds()
     else:
         for time, regime in regimes[1:].items():
-            if regime != initial_regime:
-                if not in_second_regime:
-                    total_duration_in_initial_regime += (time - start).total_seconds()
-                    in_second_regime = True
-            else:
-                if in_second_regime:
-                    start = time
-                    in_second_regime = False
+            if regime == prev
+            # if regime != initial_regime:
+            #     if not in_second_regime:
+            #         total_duration_in_initial_regime += (time - start).total_seconds()
+            #         in_second_regime = True
+            # else:
+            #     if in_second_regime:
+            #         start = time
+            #         in_second_regime = False
+
 
     total_duration = (regimes.index[-1] - regimes.index[0]).total_seconds()
+    print(total_duration_in_initial_regime,"*")
+    print(total_duration, '**')
+    print(total_duration_in_initial_regime/total_duration,'$')
+    print(initial_regime,'***')
 
     if verbose:
         print('Total duration of time: {}'.format(total_duration))
