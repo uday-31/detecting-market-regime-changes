2536b84cf25604cc5cc62bbae8526350b91bbe1f
Sarthak Vishnoi <sarthakvishnoi01@gmail.com>
2023-02-27 00:23:52 -0500

    Naive Bayes threshold

--- modules/NaiveBayesClassifier.py
+++ modules/NaiveBayesClassifier.py
@@ -4,6 +4,7 @@
 # Sarthak Vishnoi
 ##############################################################################
 import pandas as pd
+import numpy as np
 from sklearn.naive_bayes import GaussianNB
 
 
@@ -30,17 +31,27 @@ def get_predict_probs_NBC( model, X ):
 
     return model.predict_proba(X)
 
-def predict_NBC( model, X ):
+def predict_NBC( model, X, epsilon = 0.5 ):
     """
     params: model-> A fitted Naive Bayes Model
             X -> A pd dataframe of test variables
 
     return: Returns a list of predictions (highest probability) for each test point
     """
+    
+
+    if( epsilon != 0.5 ):
+        ans = np.zeros(len(X))
+        probs = get_predict_probs_NBC( model, X )
+        for i in range(len(probs)):
+            if( probs[i][1] >= epsilon ):
+                ans[i] = 1
+        return ans
+
 
     return model.predict(X)
 
-def do_all_NBC(X_train, y_train, X_valid):
+def do_all_NBC(X_train, y_train, X_valid, epsilon = 0.5):
     """
     Trains a naive bayes model with y_train as regimes after filteration and X_train as one of the three DC indicators
 
@@ -50,5 +61,5 @@ def do_all_NBC(X_train, y_train, X_valid):
     """
 
     model = train_NBC( X_train, y_train )
-    preds = predict_NBC( model, X_valid )
+    preds = predict_NBC( model, X_valid, epsilon )
     return preds
