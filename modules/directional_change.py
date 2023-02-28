4af402d85e2b686ba5f16a508575b919b35312dd
rohan <thewarlock321@gmail.com>
2023-02-27 23:09:34 -0500

    * Add function to annotate graph.

--- modules/directional_change.py
+++ modules/directional_change.py
@@ -6,15 +6,19 @@
 import pandas as pd
 import yfinance as yf
 import numpy as np
+import matplotlib as plt
+import matplotlib.dates as md
 
-def get_data(tickers, start_date, delta, end_date = '2022-12-31'):
-    data = yf.download(tickers, start=start_date, end = end_date) # time series data
+
+def get_data(tickers, start_date, delta, end_date='2022-12-31'):
+    data = yf.download(tickers, start=start_date, end=end_date)  # time series data
     df_ts_close = data['Adj Close'].dropna()
     df_ts_open = data['Open'].dropna()
-    df_ts_open.index = df_ts_open.index + pd.Timedelta(f'{delta}h') # adjust time
-    df_ts = pd.concat([df_ts_close,df_ts_open]).sort_index()
+    df_ts_open.index = df_ts_open.index + pd.Timedelta(f'{delta}h')  # adjust time
+    df_ts = pd.concat([df_ts_close, df_ts_open]).sort_index()
 
     return df_ts
+
 
 def get_pct_change(start, end):
     return (end - start) / start
@@ -65,6 +69,7 @@ def get_DC_data_v2(prices: pd.Series, theta: float) -> list[tuple]:
             last_high_time = timestamp
     return ret_val
 
+
 def get_DC_data(data: pd.Series, theta: float) -> list[tuple]:
     """Returns the Directional Change (DC) data for a given price series.
 
@@ -80,25 +85,25 @@ def get_DC_data(data: pd.Series, theta: float) -> list[tuple]:
              {DCC_time, DCC_Price, EXT_time, EXT_Price}
     """
 
-    rets = data.pct_change().dropna().to_numpy() # pct change returns
-    DCC = [] # idx for directional change confirmations
-    EXT = [] # idx for extreme points
+    rets = data.pct_change().dropna().to_numpy()  # pct change returns
+    DCC = []  # idx for directional change confirmations
+    EXT = []  # idx for extreme points
 
-    prev_sign = np.sign(rets[0]).astype(int) # store sign(return) from the previous time step
-    accumulated = rets[0] # accumulated % return
-    idx_change = 0 # index the direction changes, candidate for EXT
+    prev_sign = np.sign(rets[0]).astype(int)  # store sign(return) from the previous time step
+    accumulated = rets[0]  # accumulated % return
+    idx_change = 0  # index the direction changes, candidate for EXT
     sign_already_flagged = 0
 
-    for idx,ret in zip(range(1,len(rets)),rets[1:]):
-        
+    for idx, ret in zip(range(1, len(rets)), rets[1:]):
+
         ret_sign = np.sign(ret).astype(int)
 
-        if (ret_sign!=prev_sign):
+        if (ret_sign != prev_sign):
             # sign is different from previous time step, trend ends
-            idx_change = idx-1 # previous price point is a candidate for EXT
-            accumulated = ret # reset accumulated sum
-            
-            
+            idx_change = idx - 1  # previous price point is a candidate for EXT
+            accumulated = ret  # reset accumulated sum
+
+
         # same sign
         elif ret_sign != sign_already_flagged:
             # once we flag a threshold, we don't flag it again for the same trend
@@ -109,18 +114,19 @@ def get_DC_data(data: pd.Series, theta: float) -> list[tuple]:
                 DCC.append(idx)
                 EXT.append(idx_change)
                 sign_already_flagged = ret_sign
-        
-        prev_sign = ret_sign # set the last seen sign to the current sign
+
+        prev_sign = ret_sign  # set the last seen sign to the current sign
 
     DCC = data.iloc[1:].iloc[DCC]
     EXT = data.iloc[1:].iloc[EXT]
 
     ans = []
     for i in range(len(DCC)):
-        ans.append( (DCC.index[i], DCC[i], EXT.index[i], EXT[i]) )
+        ans.append((DCC.index[i], DCC[i], EXT.index[i], EXT[i]))
     return ans
 
-def get_DCC_EXT( DC: list ) -> tuple( (list, list, list, list) ):
+
+def get_DCC_EXT(DC: list) -> tuple((list, list, list, list)):
     """
     Get the DCC, EXT points with prices at those points and the time
 
@@ -136,13 +142,12 @@ def get_DCC_EXT( DC: list ) -> tuple( (list, list, list, list) ):
     DCC_index = []
 
     for i in range(len(DC)):
-        DCC.append( DC[i][1] )
-        DCC_index.append( DC[i][0] )
-        EXT.append( DC[i][3] )
-        EXT_index.append( DC[i][2] )
+        DCC.append(DC[i][1])
+        DCC_index.append(DC[i][0])
+        EXT.append(DC[i][3])
+        EXT_index.append(DC[i][2])
 
     return (DCC, DCC_index, EXT, EXT_index)
-
 
 
 def get_TMV(DC: list, theta: float) -> pd.Series:
@@ -155,10 +160,11 @@ def get_TMV(DC: list, theta: float) -> pd.Series:
     Returns:
         pd.Series: total price movement at respective timestamps
     """
-    _, _, ext, idx = get_DCC_EXT( DC )
+    _, _, ext, idx = get_DCC_EXT(DC)
 
-    ext = pd.Series( data = ext, index = idx )
-    return ext.pct_change().dropna()/theta
+    ext = pd.Series(data=ext, index=idx)
+    return ext.pct_change().dropna() / theta
+
 
 def get_T(DC: list) -> pd.Series:
     """Gets the time for completion of a TMV trend, in days.
@@ -171,11 +177,12 @@ def get_T(DC: list) -> pd.Series:
     """
     # extract number of days and hours between extreme points
 
-    _, _, ext, idx = get_DCC_EXT( DC )
-        
-    t_ext = pd.Series(idx).diff().dropna().apply(lambda x: x.days + (x.seconds//3600)/24)
+    _, _, ext, idx = get_DCC_EXT(DC)
+
+    t_ext = pd.Series(idx).diff().dropna().apply(lambda x: x.days + (x.seconds // 3600) / 24)
     t_ext.index = idx[1:]
     return t_ext
+
 
 def get_R(tmv: pd.Series, T: pd.Series, theta: float) -> pd.Series:
     """Gets the absolute return (R), which is the time-adjusted return of DC.
@@ -188,7 +195,63 @@ def get_R(tmv: pd.Series, T: pd.Series, theta: float) -> pd.Series:
     Returns:
         pd.Series: time-adjusted return of DC
     """
-    return tmv*theta/T  
+    return tmv * theta / T
+
+
+def annotate_plot(ax, sample_ext, sample_dcc):
+    prop1 = dict(arrowstyle="-|>,head_width=0.75,head_length=0.8",
+                 shrinkA=0, shrinkB=0, color="red", lw=2)
+    prop2 = dict(arrowstyle="-|>,head_width=0.75,head_length=0.8",
+                 shrinkA=0, shrinkB=0, color="green", lw=2)
+    prop3 = dict(arrowstyle="-", shrinkA=0, shrinkB=0, color="black", ls="dashed", lw=2)
+    prop4 = dict(arrowstyle="<-", shrinkA=0, shrinkB=0, color="black", ls="dashed", lw=2)
+    prop5 = dict(arrowstyle="->", shrinkA=0, shrinkB=0, color="black", ls="dashed", lw=2)
+
+    plt.annotate("", xytext=(sample_ext.index[0], sample_ext[0]),
+                 xy=(sample_dcc.index[0], sample_dcc[0]), arrowprops=prop1)
+
+    plt.annotate("", xytext=(sample_ext.index[0], sample_ext[0]),
+                 xy=(sample_ext.index[0], sample_dcc[0]), arrowprops=prop5)
+
+    plt.annotate("Theta", xytext=(md.date2num(sample_ext.index[0]) + 0.1, sample_dcc[0] + 5),
+                 xy=(sample_ext.index[0], sample_ext[0]), rotation=270)
+
+    plt.annotate("", xytext=(sample_dcc.index[0], sample_dcc[0]),
+                 xy=(sample_ext.index[1], sample_ext[1]), arrowprops=prop2)
+    plt.annotate("Overshoot", xytext=(sample_dcc.index[0], sample_dcc[0] - 5),
+                 xy=(sample_ext.index[1], sample_ext[1]))
+
+    plt.annotate("", xytext=(sample_ext.index[1], sample_ext[1]),
+                 xy=(sample_dcc.index[1], sample_dcc[1]), arrowprops=prop1)
+    plt.annotate("", xytext=(sample_dcc.index[1], sample_dcc[1]),
+                 xy=(sample_ext.index[2], sample_ext[2]), arrowprops=prop2)
+
+    plt.annotate("Overshoot", xytext=(md.date2num(sample_dcc.index[1]) + 2, sample_dcc[1] + 5),
+                 xy=(sample_ext.index[2], sample_ext[2]))
+
+    plt.annotate("", xytext=(sample_ext.index[0], sample_dcc[0]),
+                 xy=(sample_dcc.index[0], sample_dcc[0]), arrowprops=prop5)
+
+    plt.annotate("", xytext=(sample_ext.index[1], sample_ext[1]),
+                 xy=(sample_dcc.index[1], sample_ext[1]), arrowprops=prop5)
+
+    plt.annotate("", xytext=(sample_dcc.index[1], sample_ext[1]),
+                 xy=(sample_dcc.index[1], sample_dcc[1]), arrowprops=prop5)
+
+    plt.annotate("Theta", xytext=(md.date2num(sample_dcc.index[1]) + 0.2, sample_ext[1] + 5),
+                 xy=(sample_dcc.index[1], sample_dcc[1]), rotation=90)
+
+    plt.annotate("", xytext=(sample_dcc.index[1], sample_ext[1]),
+                 xy=(sample_ext.index[2], sample_ext[1]), arrowprops=prop5)
+
+    plt.annotate("", xytext=(sample_ext.index[2], sample_ext[1]),
+                 xy=(sample_ext.index[2], sample_ext[2]), arrowprops=prop5)
+
+    ax.axvspan(sample_ext.index[0], sample_ext.index[1], color='red', alpha=0.25)
+    ax.axvspan(sample_ext.index[1], sample_ext.index[2], color='green', alpha=0.25)
+
+    ax.text((md.date2num(sample_ext.index[0]) + 2), y=1420, s='Downward Trend')
+    ax.text((md.date2num(sample_ext.index[1]) + 2), y=1420, s='Upward Trend')
 
 
 if __name__ == '__main__':
